15c036431bc4658bf9e6212112b274cfcc8ffd0c
Mladen Nedev <mlados43@gmail.com>
2023-01-23 16:34:07 +0200

    Initial commit

---
+++ src/templates.js
@@ -0,0 +1,37 @@
+export const homeTemplate
+    = `<div id="home">
+    <h2>Welcome, nice to meet you</h2>
+</div>`;
+
+export const aboutTemplate
+    = `<div id="about">
+    <h2>About Me</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione numquam dolores maxime asperiores velit
+        commodi porro optio ipsam, error rerum fugit non mollitia et earum, nesciunt accusantium eius esse
+        doloremque voluptatem quos consequuntur, itaque repellat? Voluptatem amet, minus ex porro unde ducimus
+        dolores maiores, ipsum laborum alias deserunt libero a.
+    </p>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero possimus officia deserunt incidunt
+        voluptatem eligendi dignissimos quo tempora eos iure.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui animi natus rerum, nam quas quia!
+    </p>
+</div>`;
+
+export const skillsTemplate
+    = `<div id="skills">
+    <h2>My skills</h2>
+</div>`;
+
+export const coursesTemplate
+    = `<div id="courses">
+    <h2>My courses</h2>
+</div>`;
+
+export const contactTemplate
+    = `<div id="contacts">
+    <h2>Contact me</h2>
+</div>`;
---
+++ src/util.js
@@ -0,0 +1,67 @@
+import {
+    homeTemplate,
+    aboutTemplate,
+    skillsTemplate,
+    coursesTemplate,
+    contactTemplate
+} from './templates.js';
+
+const containerEl = document.getElementById('container');
+const navEls = document.querySelectorAll('.navigation-section nav ul li button');
+
+export function ValidateContent(button) {
+    if (button.textContent == 'Home') {
+        containerEl.innerHTML = homeTemplate;
+    } else if (button.textContent == 'About Me') {
+        containerEl.innerHTML = aboutTemplate;
+    } else if (button.textContent == 'Skills') {
+        containerEl.innerHTML = skillsTemplate;
+    } else if (button.textContent == 'Courses') {
+        containerEl.innerHTML = coursesTemplate;
+    } else if (button.textContent == 'Contacts') {
+        containerEl.innerHTML = contactTemplate;
+    }
+    ActiveNavCheck();
+}
+
+export function ActiveNavCheck() {
+    const containerChildEl = document.querySelector('#container > div');
+    RemoveActiveNav();
+
+    if (containerChildEl.id == 'home') {
+        navEls[0].classList.add('active');
+    } else if (containerChildEl.id == 'about') {
+        navEls[1].classList.add('active');
+    } else if (containerChildEl.id == 'skills') {
+        navEls[2].classList.add('active');
+    } else if (containerChildEl.id == 'courses') {
+        navEls[3].classList.add('active');
+    } else if (containerChildEl.id == 'contacts') {
+        navEls[4].classList.add('active');
+    }
+};
+
+function RemoveActiveNav() {
+    for (const button of navEls) {
+        button.classList.remove('active');
+    }
+};
+
+function DisableAllButtons() {
+    for (const button of navEls) {
+        button.disabled = true;
+    }
+};
+
+function EnableAllButtons() {
+    for (const button of navEls) {
+        button.disabled = false;
+    }
+};
+
+export function SuspendNavButtons() {
+    DisableAllButtons();
+    setTimeout(() => {
+        EnableAllButtons();
+    }, 1200);
+}
